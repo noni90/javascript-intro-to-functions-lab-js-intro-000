b917f95b67eca4d7d13ee212c07137a1a6c59333
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-15 19:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function logWhisper(string)
 {
   console.log(string.toLowerCase());
 }
+function sayHiToGrandma(string)
+{
+  var lower = string;
+  var upper = string;
+  if(string.toLowerCase() === lower){
+  return "I can't hear you!";
+  }
+  else if(string.toUpperCase() === upper){
+  return "YES INDEED!";
+   }
+   var tt="I love you, Grandma.";
+  else if (string === tt){
+    return "I love you, too.";
+  }
+}
